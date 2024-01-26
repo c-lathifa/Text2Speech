8c59e4745b0f9f8255319ff86018a0832bd1b4c0
c-lathifa <83398736+c-lathifa@users.noreply.github.com>
2024-01-26 19:36:52 +0530

    Update settings.py

--- TextToSpeech/settings.py
+++ TextToSpeech/settings.py
@@ -81,10 +81,9 @@ WSGI_APPLICATION = 'TextToSpeech.wsgi.application'
 
 
 DATABASES = {
-DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3")
     }
 }
 
